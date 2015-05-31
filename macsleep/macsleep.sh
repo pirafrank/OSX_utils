1fb452f687ae46adcba8096eab9f314ac3c91ec7
Francesco Pira <dev@fpira.com>
2015-05-31 13:21:03 +0200

    license fix

--- macsleep/macsleep.sh
+++ macsleep/macsleep.sh
@@ -8,18 +8,18 @@
 #
 # This file is part of OSX_utils
 #
-# This script is free software: you can redistribute it and/or modify
+# OSX_utils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This script is distributed in the hope that it will be useful,
+# OSX_utils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
+# along with OSX_utils. If not, see <http://www.gnu.org/licenses/>.
 #
 ################################################################################
 
--- nosleep/nosleep.sh
+++ nosleep/nosleep.sh
@@ -8,20 +8,20 @@
 #
 # This file is part of OSX_utils
 #
-# This script is free software: you can redistribute it and/or modify
+# OSX_utils is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This script is distributed in the hope that it will be useful,
+# OSX_utils is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
+# along with OSX_utils. If not, see <http://www.gnu.org/licenses/>.
 #
-#############################################################################
+################################################################################
 
 let "MIN=$1 * 60"
 echo "The system will stay awake for $1 minutes since now ($(date))."
